93b97a7362a8aba20ea4ec0ef1eaa5e6e001f8e3
Ifee1 <ifechukwudee582@gmail.com>
2024-12-04 12:25:50 +0100

    Final linking and touch ups

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  scrollToSection(sectionId: string): void {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
 }
